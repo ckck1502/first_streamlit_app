fa1c0cf7c0cdf5a7235eac56b1cf2f21ce5cea3e
ckck1502 <141426873+ckck1502@users.noreply.github.com>
2023-08-04 21:45:33 +0200

    adding breakfast information

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach')
-streamlit.text('Hard Boiled Free Range Egg')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach')
+streamlit.text('🐔 Hard Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
